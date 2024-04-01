1e1c04e5b4f3e7cae511660adc4d22246e0d50ff
fabriciocod <fva.mcz@gmail.com>
2024-04-01 16:28:13 -0300

    Adiciona Id ao Cat e cria uma tela para exibir Cat

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const getNomeCompleto = (nome, sobrenome) =>{
   return nome + ' ' + sobrenome;
 }
 
-const Cat = ({nome, sobrenome, idade}) =>{
+const Cat = ({id, nome, sobrenome, idade}) =>{
   // const name = "Gafield"
 
   // let texto ="anos";
@@ -17,7 +17,13 @@ const Cat = ({nome, sobrenome, idade}) =>{
 
   const texto = (idade <= 1) ? "ano" : "anos" // if ternário utilizar apenas quando houver apenas uma condição para afirmação.
 
-  return <Text>Olá, Eu sou {getNomeCompleto(nome, sobrenome).toUpperCase()} tenho {idade} {texto}!</Text>
+  return (
+    <Link href={{
+      pathname: "/gatos/[id]",
+      params:{id: id}
+    }}>Olá, Eu sou {getNomeCompleto(nome, sobrenome).toUpperCase()} tenho {idade} {texto}! E meu id {id}</Link>
+  
+  );
 }
 
 function IFAL() {
@@ -28,10 +34,10 @@ function IFAL() {
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text>
       <Text>Inicalização do App em React Native</Text> */}
-      <Cat nome="Black" sobrenome="Moon" idade={4}/>
-      <Cat nome="Garfilde" sobrenome="3°" idade={45}/>
-      <Cat nome="Apolo" sobrenome="Grego" idade={8}/>
-      <Cat nome="Mandachuva" sobrenome="trambiqueiro" idade={1}/>
+      <Cat id={1} nome="Black" sobrenome="Moon" idade={4}/>
+      <Cat id={2}nome="Garfilde" sobrenome="3°" idade={45}/>
+      <Cat id={3}nome="Apolo" sobrenome="Grego" idade={8}/>
+      <Cat id={4} nome="Mandachuva" sobrenome="trambiqueiro" idade={1}/>
       <StatusBar style="auto" />
 
       <Link href="/cliques">Cliques</Link>
